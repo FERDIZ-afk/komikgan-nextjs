8406d83779a27521ec05f5611a032cb9850c6b40
FERDIZ-afk <ferdizakyla@gmail.com>
2025-02-09 19:38:41 +0700

    千夜未来

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,8 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-const client = hc<AppType>("http://localhost:3000/")
+//const client = hc<AppType>("http://localhost:3000/")
+const client = hc<AppType>("https://komikgan-nextjs.vercel.app")
 const api = client.api
 
 export default api
